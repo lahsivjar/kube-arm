9c5f7e6ec728ded9ba786097cb4981b4fa3c8533
Vishal Raj <lahsivjar@gmail.com>
2017-01-28 21:12:13 +0800

    Build basic service registry framework.

--- essentials/jarvis-system/orchestrator/src/constants.py
+++ essentials/jarvis-system/orchestrator/src/constants.py
@@ -5,3 +5,12 @@ class ResponseType(Enum):
   STATE = 'state'
   REDIRECT = 'redirect'
   FORWARD = 'forward'
+
+class RegistryKeys(Enum):
+  SERVICE_ID = 'service_id'
+  SERVICE_NAME = 'service_name'
+  SERVICE_PORT = 'service_port'
+  NAMESPACE = 'namespace'
+  ENDPOINTS = 'endpoints'
+  URI = 'uri'
+  ACCEPTANCE_REGEX = 'acceptance_regex'
--- essentials/jarvis-system/orchestrator/src/controller.py
+++ essentials/jarvis-system/orchestrator/src/controller.py
@@ -1,11 +1,21 @@
 import queryanalyzer
+import serviceregistry
+import json
 
+from serviceregistry import ServiceRegistry
 from flask import Flask
 from flask import request
 
 mapping = Flask(__name__)
 
-@mapping.route("/v1", methods=['GET'])
+@mapping.route('/v1', methods=['GET'])
 def query():
   query = request.args.get('query')
   return queryanalyzer.do(query)
+
+@mapping.route('/v1/register', methods=['POST'])
+def register():
+  # Get the service description
+  service_desc = request.json
+  # Register service
+  return json.dumps(ServiceRegistry().register(service_desc))
