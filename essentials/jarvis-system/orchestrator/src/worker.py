c9f003db40600f5401d01878524ad473ac65c6ed
Vishal Raj <lahsivjar@gmail.com>
2017-01-30 04:54:40 +0800

    Fix worker class to work in case of no service registered.

--- essentials/jarvis-system/orchestrator/src/worker.py
+++ essentials/jarvis-system/orchestrator/src/worker.py
@@ -15,23 +15,24 @@ class ServiceResolver:
   def get_resolved_services(self, query):
     regex_map = self.registry.get_regex_map()
     service_list = []
-    for uri, value in regex_map.iteritems():
-      service_id = value.get(RegistryKeys.SERVICE_ID)
-      regex = value.get(RegistryKeys.ACCEPTANCE_COMPILED_REGEX)
+    if regex_map != None:
+      for uri, value in regex_map.iteritems():
+        service_id = value.get(RegistryKeys.SERVICE_ID)
+        regex = value.get(RegistryKeys.ACCEPTANCE_COMPILED_REGEX)
 
-      # Evaluate regex if matches then add the service to the list
-      match = regex.match(query)
-      if match != None:
-        service_dict = self.registry.get(service_id)
-        assert service_dict != None
-        service_list.append({
-            RegistryKeys.SERVICE_ID: service_dict[RegistryKeys.SERVICE_ID],
-            RegistryKeys.SERVICE_NAME: service_dict[RegistryKeys.SERVICE_NAME],
-            RegistryKeys.NAMESPACE: service_dict[RegistryKeys.NAMESPACE],
-            RegistryKeys.SERVICE_PORT: service_dict[RegistryKeys.SERVICE_PORT],
-            RegistryKeys.URI: uri,
-            RegistryKeys.FILTER_COMPILED_REGEX: value.get(RegistryKeys.FILTER_COMPILED_REGEX)
-        })
+        # Evaluate regex if matches then add the service to the list
+        match = regex.match(query)
+        if match != None:
+          service_dict = self.registry.get(service_id)
+          assert service_dict != None
+          service_list.append({
+              RegistryKeys.SERVICE_ID: service_dict[RegistryKeys.SERVICE_ID],
+              RegistryKeys.SERVICE_NAME: service_dict[RegistryKeys.SERVICE_NAME],
+              RegistryKeys.NAMESPACE: service_dict[RegistryKeys.NAMESPACE],
+              RegistryKeys.SERVICE_PORT: service_dict[RegistryKeys.SERVICE_PORT],
+              RegistryKeys.URI: uri,
+              RegistryKeys.FILTER_COMPILED_REGEX: value.get(RegistryKeys.FILTER_COMPILED_REGEX)
+          })
     return service_list
 
 class Worker:
@@ -40,20 +41,21 @@ class Worker:
 
   def do(self, query):
     service_list = self.resolver.get_resolved_services(query)
-    urls = []
-    response_list = []
-    for service in service_list:
-      filtered_query = self._filter_query(query, service[RegistryKeys.FILTER_COMPILED_REGEX])
-      urls.append(self._get_url(service, filtered_query))
+    if service_list:
+      urls = []
+      response_list = []
+      for service in service_list:
+        filtered_query = self._filter_query(query, service[RegistryKeys.FILTER_COMPILED_REGEX])
+        urls.append(self._get_url(service, filtered_query))
 
-    # Create a set of unsent requests
-    response_set = (grequests.get(url) for url in urls)
-    # Send the requests
-    response_array = grequests.map(response_set)
-    # Get the response content from the response and close the response
-    for (response in response_array):
-      response_list.append(response.json())
-      response.close()
+      # Create a set of unsent requests
+      response_set = (grequests.get(url) for url in urls)
+      # Send the requests
+      response_array = grequests.map(response_set)
+      # Get the response content from the response and close the response
+      for response in response_array:
+        response_list.append(response.json())
+        response.close()
     return response_list
 
   def _get_url(self, service, query):
