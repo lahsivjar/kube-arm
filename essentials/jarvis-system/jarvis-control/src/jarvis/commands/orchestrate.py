00b7a86ac6c7d72f5ac8c281e79649c38f7e8176
Bharath Bhat <bharathb777@gmail.com>
2017-02-01 23:09:10 +0800

    module registration occurs on deploy

--- essentials/jarvis-system/jarvis-control/src/jarvis/commands/orchestrate.py
+++ essentials/jarvis-system/jarvis-control/src/jarvis/commands/orchestrate.py
@@ -3,11 +3,12 @@
 import json
 import urllib2
 
+# TODO get config from central location
 ORCHESTRATOR_PROTOCOL = 'http'
-#ORCHESTRATOR_HOST = '119.74.248.86'
-ORCHESTRATOR_HOST = '192.168.1.11'
+ORCHESTRATOR_HOST = '119.74.248.86'
+# ORCHESTRATOR_HOST = '192.168.1.11'
 ORCHESTRATOR_PORT = '8080'
-ORCHESTRATOR_PATH = '/jarvis/orchestrator/v1?query=%s'
+ORCHESTRATOR_QUERY_PATH = 'jarvis/orchestrator/v1?query=%s'
 
 def available_arguments():
 	return [
@@ -21,8 +22,7 @@ def available_arguments():
 
 def run(args):
 	task = ' '.join(args.task)
-	print 'Let me try to %s for you...' % task
-	request_url = (ORCHESTRATOR_PROTOCOL + '://' + ORCHESTRATOR_HOST +':' + ORCHESTRATOR_PORT + ORCHESTRATOR_PATH) % urllib2.quote(task)
+	request_url = '%s://%s:%s/%s' % (ORCHESTRATOR_PROTOCOL, ORCHESTRATOR_HOST, ORCHESTRATOR_PORT, (ORCHESTRATOR_QUERY_PATH % urllib2.quote(task)))
 	response = urllib2.urlopen(request_url)
 	response_data = json.load(response)
 	print response_data
