c84f4d50bc8591c1b07316d61e0a88eea1577511
Vishal Raj <lahsivjar@gmail.com>
2017-02-03 01:11:49 +0800

    Use hash interface of redis and improve implementation.

--- essentials/jarvis-system/orchestrator/src/serviceregistry.py
+++ essentials/jarvis-system/orchestrator/src/serviceregistry.py
@@ -12,11 +12,8 @@ from voluptuous import Schema, Required, Any, MultipleInvalid
 REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PORT = os.environ['REDIS_PORT']
 
-class _RedisEntryType(Enum):
-  SERVICE_REGISTRY = 'service_registry'
-  REGEX_MAP = 'regex_map'
-
-_KEY_SEPARATOR = '::'
+# Redis key for service registry
+_SERVICE_REGISTRY = 'service_registry'
 
 class ServiceRegistry:
   def __init__(self):
@@ -41,21 +38,39 @@ class ServiceRegistry:
     try:
       self.validateschema(service_dict)
       service_id = service_dict[RegistryKeys.SERVICE_ID]
-      service_key = self._create_service_key(service_id)
 
-      # Update the regex key list
-      self._update_regex_list(service_id,service_dict[RegistryKeys.ENDPOINTS]);
-      self.redisinstance.set(service_key, self._serialize_data(service_dict))
+      # Compile each endpoint regex
+      endpoints = service_dict[RegistryKeys.ENDPOINTS]
+      for endpoint in endpoints:
+        a_regex = endpoint.pop(RegistryKeys.ACCEPTANCE_REGEX)
+        f_regex = endpoint.pop(RegistryKeys.FILTER_REGEX, None)
+        compiled_a_regex = re.compile(a_regex)
+        if f_regex:
+          compiled_f_regex = {
+            RegistryKeys.COMPILED_PATTERN: re.compile(f_regex.pop(RegistryKeys.PATTERN), re.I),
+            RegistryKeys.REPLACE: f_regex.pop(RegistryKeys.REPLACE, None)
+          }
+
+        # Update the endpoint object
+        endpoint[RegistryKeys.ACCEPTANCE_COMPILED_REGEX] = compiled_a_regex
+        endpoint[RegistryKeys.FILTER_COMPILED_REGEX] = compiled_f_regex
+
+      self.redisinstance.hset(_SERVICE_REGISTRY, service_id, self._serialize_data(service_dict))
       return True
     except MultipleInvalid as e:
       return False
 
   def get(self, service_id):
-    service_key = self._create_service_key(service_id)
-    return self._deserialize_data(self.redisinstance.get(service_key))
+    return self._deserialize_data(self.redisinstance.hget(_SERVICE_REGISTRY, service_id))
 
-  def get_regex_map(self):
-    return self._deserialize_data(self.redisinstance.get(_RedisEntryType.REGEX_MAP))
+  def getall(self):
+    all_services_map = self.redisinstance.hgetall(_SERVICE_REGISTRY)
+    all_services_list = all_services_map.values() if all_services_map else []
+    deserialized_all_services_list = []
+    for service in all_services_list:
+      deserialized_all_services_list.append(self._deserialize_data(service))
+
+    return deserialized_all_services_list
 
   def clear(self):
     self.redisinstance.flushdb()
@@ -65,42 +80,3 @@ class ServiceRegistry:
 
   def _deserialize_data(self, data):
     return None if data == None else pickle.loads(data)
-
-  def _create_service_key(self, rawkey):
-    keybuilder = [_RedisEntryType.SERVICE_REGISTRY]
-    keybuilder.append(_KEY_SEPARATOR)
-    keybuilder.append(rawkey)
-
-    return string.join(keybuilder, '')
-
-  def _update_regex_list(self, service_id, endpoints_list):
-    re_map = self.get_regex_map()
-    re_map = {} if re_map == None else re_map
-    for endpoint in endpoints_list:
-      a_regex = endpoint[RegistryKeys.ACCEPTANCE_REGEX]
-      uri = endpoint[RegistryKeys.URI]
-
-      re_map_entry = {
-        RegistryKeys.ACCEPTANCE_COMPILED_REGEX: re.compile(a_regex),
-        RegistryKeys.SERVICE_ID: service_id
-      }
-
-      # Compile the filter regex
-      if RegistryKeys.FILTER_REGEX in endpoint:
-        f_regex_map = endpoint[RegistryKeys.FILTER_REGEX]
-        f_regex = re.compile(f_regex_map[RegistryKeys.PATTERN], re.I) if f_regex_map != None else None
-        f_repl_regex = f_regex_map[RegistryKeys.REPLACE] if f_regex != None else ''
-
-        if f_regex != None:
-          re_map_entry.update({
-            RegistryKeys.FILTER_COMPILED_REGEX: {
-              RegistryKeys.COMPILED_PATTERN: f_regex,
-              RegistryKeys.REPLACE: f_repl_regex
-            }
-          })
-
-      # Add the new re map entry to the existing re map
-      re_map.update({ uri: re_map_entry })
-
-    # Update to the new list in redis
-    self.redisinstance.set(_RedisEntryType.REGEX_MAP, self._serialize_data(re_map))
