43b3c3bec26d0d40dd74f4d6fe89e224fefc8212
Vishal Raj <lahsivjar@gmail.com>
2017-02-03 23:36:06 +0800

    refs #18 Change url mapping and add endpoint to get all services.

--- essentials/jarvis-system/orchestrator/src/controller.py
+++ essentials/jarvis-system/orchestrator/src/controller.py
@@ -7,21 +7,25 @@ from flask import request
 
 mapping = Flask(__name__)
 
-@mapping.route('/v1', methods=['GET'])
+@mapping.route('/v1/search', methods=['GET'])
 def query():
   query = request.args.get('query')
   return json.dumps(Worker().do(query))
 
-@mapping.route('/v1/register', methods=['POST'])
+@mapping.route('/v1/service/register', methods=['POST'])
 def register():
   # Get the service description
   service_desc = request.json
   # Register service
   return json.dumps(ServiceRegistry().register(service_desc))
 
-@mapping.route('/v1/deregister', methods=['POST'])
+@mapping.route('/v1/service/deregister', methods=['POST'])
 def deregister():
   # Get service id to deregister
   service_id = request.form['service_id']
   # Deregister the service
   return json.dumps(ServiceRegistry().deregister(service_id))
+
+@mapping.route('/v1/service/getall', methods=['GET'])
+def get_all_services():
+  return json.dumps(ServiceRegistry().get_service_list())
